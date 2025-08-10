9929f3d516232c16763a8a184a6adb353f304e14
somayesh73-glitch <somaye.sh73@gmail.com>
2025-08-10 19:46:25 +0330

    Update bot.py
    fix

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ async def price(update: Update, context: ContextTypes.DEFAULT_TYPE):
     now_tehran = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
     await update.message.reply_text(f"Current Gold Price: {gold_price} USD Signal: {signal}Time (Tehran): {now_tehran}")
 
-if name == "main":
+if __name__ == "__main__":
     app = ApplicationBuilder().token("YOUR_BOT_TOKEN_HERE").build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("price", price))
